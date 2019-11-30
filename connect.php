60f60982dddc8cb542f46b7149937c3c3388dc40
Becky <243594043@qq.com>
2019-11-30 20:08:20 +0800

    connect

--- connect.php
+++ connect.php
@@ -1,10 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$host = 'localhost';
+$host = '127.0.0.1';
 $user = 'root';
 $pass = '';
-$con = mysqli_connect($host, $user, $pass);
-mysqli_select_db($con,"library");
+$con = mysqli_connect($host, $user, $pass,"library");
 mysqli_query($con,"SET names UTF8");
 mysqli_set_charset($con,"utf8");
 $timezone="Asia/Shanghai";
